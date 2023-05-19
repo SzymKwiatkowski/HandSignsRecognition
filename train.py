2e8481a7bd39b869129e5fed42eba4c94b22fd98
Szymon Kwiatkowski <szymon.z.kwiatkowski@gmail.com>
2023-05-19 20:24:34 +0200

    Added some comments to code

--- train.py
+++ train.py
@@ -17,11 +17,6 @@ from sklearn.inspection import DecisionBoundaryDisplay
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import os 
 
-hand_dict = {
-    'Right': 0,
-    'Left': 1
-}
-
 letter_dict = {
     'a': 0,
     'b': 1,
@@ -49,39 +44,64 @@ letter_dict = {
     'y': 23,
 }
 
-
-
 def main():
+    # set run flags
+    testing = False
+    save_model = False
+    
+    # load preprocessed training data
     data = pickle.load(open('data/data.pickle', 'rb'))
-    # print(data)
     x = data['data']
     y = data['labels']
+    
+    # Convert to numpy array
     x = x.to_numpy()
+    
+    # Set filename for model to get save to or read from
     filename = 'models/finalized_model.sav'
-    testing = True
+
+    # Split data evenly thorought with 8 to 2 proportion
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True, random_state=42, stratify=y)
     
-    # X_train, X_calib, y_train, y_calib = train_test_split(X_train, y_train, test_size=0.2, shuffle=True, random_state=42, stratify=y_train)
-    if not os.path.isfile(filename) and not testing:
+    if testing:
+        # Set polynominal SVC and it's properties
         clf2 = SVC(kernel='poly',probability=True, degree=4, C=12.0)
+        
+        # Neural network with custom params
         clf1 = MLPClassifier(solver='adam', activation='tanh', learning_rate='adaptive', alpha=1e-5, random_state=42, 
                              shuffle=True, batch_size=16, max_iter=1000, learning_rate_init=0.0008, power_t=0.7)
 
+        # Set VotingClassifier to ensamble both classifiers above
         eclf = VotingClassifier(estimators=[("mlpc", clf1), ('svc', clf2)],
                                 voting='soft',
                                 weights=[2, 2])
 
+        # Train and predict
         eclf.fit(X_train, y_train)
         y_predict = eclf.predict(X_test)
 
+        # Display accuracy along with score and confusion matrix
         print(accuracy_score(y_test, y_predict))
         print(precision_score(y_test, y_predict, average=None))
         cm = confusion_matrix(y_test, y_predict)
         disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=letter_dict.keys())
         disp.plot()
         plt.show()
-        if not testing:
+        
+        # Save data if flags are set and file does not exist
+        if save_model and not os.path.isfile(filename):
             pickle.dump(eclf, open(filename, 'wb'))
     
+    else:
+        # Load existing model and run tests on test data
+        model = pickle.load(open(filename, 'rb'))
+        
+        y_predict = model.predict(X_test)
+        print(accuracy_score(y_test, y_predict))
+        print(precision_score(y_test, y_predict, average=None))
+        cm = confusion_matrix(y_test, y_predict)
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=letter_dict.keys())
+        disp.plot()
+        plt.show()
 if __name__ == "__main__":
     main()
